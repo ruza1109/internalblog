23cd806c16c7c365dd959bad9402ecb7497d11f6
Igor Milosavljevic <igor.milosavljevic.1994@gmail.com>
2020-03-16 17:36:01 +0100

    Changed typo

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ use App\Database\Database;
 
 require __DIR__ . '/vendor/autoload.php';
 
-require __DIR__ . '/src/config/helpers.php';
+require __DIR__ . '/src/Config/helpers.php';
 
 dd(Database::getInstance()); //TODO Remove after testing
