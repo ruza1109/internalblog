e9155765a7b8f4c24793dfdbdb6ac16977f7b891
Igor Milosavljevic <igor.milosavljevic.1994@gmail.com>
2020-03-25 11:23:42 +0100

    testing finished

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
 
-use App\Database\Database;
-
 require __DIR__ . '/vendor/autoload.php';
-
 require __DIR__ . '/src/Config/helpers.php';
-
-dd(Database::getInstance()); //TODO Remove after testing
