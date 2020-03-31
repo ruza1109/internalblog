604c1fa50ac735eb7505788dc675b3b511ad364e
Igor Milosavljevic <igor.milosavljevic.1994@gmail.com>
2020-03-31 18:10:44 +0200

    Changed Exception classes

--- src/Exception/AbstractException.php
+++ src/Exception/AbstractException.php
@@ -10,4 +10,24 @@ abstract class AbstractException extends \Exception
     {
         parent::__construct($message, $code, $previous);
     }
+
+    protected function storeToLog()
+    {
+        $logFile = "logs/error_log.log";
+        $date = date('d-M-Y H:i:s e');
+        $message = "[$date]" . "\n" .
+                    "Message: " . $this->getMessage() . "\n" .
+                    "Code: " . $this->getCode() . "\n" .
+                    "File: " . $this->getFile() . "\n" .
+                    "Line: " . $this->getLine() . "\n" .
+                    "Trace: " . $this->getTraceAsString() . "\n\n";
+
+        file_put_contents($logFile, $message, FILE_APPEND);
+    }
+
+    public function handleError()
+    {
+        $this->storeToLog();
+        die('Sorry, something went wrong!');
+    }
 }
